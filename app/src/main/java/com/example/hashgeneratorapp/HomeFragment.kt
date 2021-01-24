c5ee9d4e186915c9dd1aa32f37806ecd5a896938
Yann Bemba Longa <yannbembacontact@gmail.com>
2021-01-24 19:15:08 +0100

    Home Fragment Animations

--- app/src/main/java/com/example/hashgeneratorapp/HomeFragment.kt
+++ app/src/main/java/com/example/hashgeneratorapp/HomeFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.hashgeneratorapp.databinding.FragmentHomeBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -12,7 +16,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+        savedInstanceState: Bundle?): View {
 
         _binding = FragmentHomeBinding.inflate(layoutInflater)
 
@@ -23,8 +27,16 @@ class HomeFragment : Fragment() {
             requireContext(),
             R.layout.drop_down_items,
             hashAlgorithms)
-
         binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+        // Animations
+
+        binding.generateBtn.setOnClickListener {
+            lifecycleScope.launch {
+                applyAnimations()
+                navigateToSuccess()
+            }
+        }
 
         return binding.root
     }
@@ -33,9 +45,53 @@ class HomeFragment : Fragment() {
         inflater.inflate(R.menu.home_menu, menu)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+    private suspend fun applyAnimations() {
+
+        val DURATION = 400L
+
+        binding.titleTextView.animate()
+            .alpha(0f)
+            .duration = DURATION
+
+        binding.view.animate()
+            .alpha(0f)
+            .duration = DURATION
+
+        binding.generateBtn.animate()
+            .alpha(0f)
+            .duration = DURATION
+
+        binding.textInputLayout.animate()
+            .alpha(0f)
+            .translationXBy(1200f)
+            .duration = DURATION
+
+        binding.plainText.animate()
+            .alpha(0f)
+            .translationXBy(-1200f)
+            .duration = DURATION
+
+        delay(300)
+
+        binding.successBg.animate().alpha(1f).duration = 600L
+        binding.successBg.animate().rotationBy(100f).duration = 600L
+        binding.successBg.animate().scaleXBy(900f).duration = 800L
+        binding.successBg.animate().scaleYBy(900f).duration = 600L
+
+        delay(500)
+
+        binding.successImg.animate().alpha(1f).duration = 2000L
+
+        delay(1500L)
+
     }
 
+    private fun navigateToSuccess(){
+        findNavController().navigate(R.id.action_homeFragment_to_successFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
